38e828e2b178188f9ad78b7490ee19fd972252aa
saufth <saufth@gmail.com>
2024-02-16 12:50:46 -0600

    Fix typo in AboutPage component

--- src/app/(lobby)/nosotros/page.tsx
+++ src/app/(lobby)/nosotros/page.tsx
@@ -41,7 +41,7 @@ export default function AboutPage () {
               </span>
             </h2>
             <p className='f-subhead-2 text-muted-foreground text-balance mt-spacing-3'>
-              Los valores de TERRAT están integrados se ven reflejados en todas sus acciones y decisiones.
+              Los valores de TERRAT están integrados y se ven reflejados en todas sus acciones y decisiones.
             </p>
           </div>
           <div className='cols-container gap-y-gutter mt-spacing-6'>
